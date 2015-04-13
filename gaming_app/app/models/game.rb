fa22700da311f97fff6b0e7ce5c67e3b2b3d1e64
spencermeyer <spencer_meyer@hotmail.com>
2015-04-13 12:04:39 +0100

    some validations added

--- gaming_app/app/models/game.rb
+++ gaming_app/app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ActiveRecord::Base
+  validates :diff, inclusion: { in: %w(1 2 3 4 5), message: "1-5"} 
   belongs_to_many :Users
   has_many :Moves
 end
--- gaming_app/app/models/move.rb
+++ gaming_app/app/models/move.rb
@@ -1,3 +1,4 @@
 class Move < ActiveRecord::Base
+  validates :value, inclusion: { in: %w(0 X), message: "Xor0 Xor0 Xor0 !!!!!"}
   belongs_to :Games
 end
