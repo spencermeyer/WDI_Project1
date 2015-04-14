d33246aa6afd9cc6618e0324bafa3b4a82b7a55a
spencermeyer <spencer_meyer@hotmail.com>
2015-04-14 13:13:24 +0100

    added icon bar

--- gaming_app/app/models/move.rb
+++ gaming_app/app/models/move.rb
@@ -2,8 +2,5 @@ class Move < ActiveRecord::Base
   validates :value, inclusion: { in: %w(0 X), message: "Xor0 Xor0 Xor0 !!!!!"}
   validates :number, inclusion: { in: (0...9).to_a}
   belongs_to :game
-
-
-
 end
 
