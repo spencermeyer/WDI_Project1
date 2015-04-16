c4c5c58f45c1be00c4d12ca82988b06934ea269c
spencermeyer <spencer_meyer@hotmail.com>
2015-04-16 10:23:59 +0100

    got results sucked into an array

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,65 +9,29 @@ class Game < ActiveRecord::Base
   end
 
   def winner
-    test = ["","","","","","","","",""]
+    winning_combinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    test = []
     i=0
-    while i<9 do
-      if(moves.find_by_number(i).present?)
-        test[i]=moves.find_by_number(i).value
+    while i<8
+      if (moves.find_by_number(i).present?)
+        test[i] = moves.find_by_number(i).value
       end
       i+=1
     end
-    testx = [
-      ["x", "x", "x", "","","","","",""],
-      ["", "", "", "x","x","x","","",""],
-      ["", "", "", "","","","x","x","x"],
-      ["x", "", "", "x","","","x","",""],
-      ["", "x", "", "","x","","","x",""],
-      ["", "", "x", "","","x","","","x"],
-      ["x", "", "", "","x","","","","x"],
-      ["", "", "x", "","x","","x","",""]
-    ]
-    test0 = [
-      ["0", "0", "0", "","","","","",""],
-      ["", "", "", "0","0","0","","",""],
-      ["", "", "", "","","","0","0","0"],
-      ["0", "", "", "0","","","0","",""],
-      ["", "0", "", "","0","","","0",""],
-      ["", "", "0", "","","0","","","0"],
-      ["0", "", "", "","0","","","","0"],
-      ["", "", "0", "","0","","0","",""]
-    ]
-    sumx = 0
-    maxsumx = 0
-    maxsum0 = 0
-    i=0
-    while i< 8 do
-      j=0
-      while j < 9 do
-        if (test[j]!="" && (test[j] == testx[i][j]))
-         sumx = sumx+1
-         if(sumx>maxsumx)
-          maxsumx=sumx
-        end
-       end
-       if (test[j]!="" && (test[j] == test0[i][j]))
-        sum0 = sum0+1
-         if(sum0>maxsum0)
-          maxsum0=sum0
-        end
-      end
-      j+=1
-    end
-    i+=1
-     # puts "total x is", sumx, "total 0 is", sum0, "for match", i
-     sumx=0
-     sum0=0
-     return maxsumx
-   end
- end
-   
+  while i<8
 
 
+
+
+    test
+  end
+  
+
+
+
+
+# when player crosses have X cells that he played
+    # find the index of the cells he played, then compare the index to the winning_combinations 
 
   # First move is always x
   def next_value
